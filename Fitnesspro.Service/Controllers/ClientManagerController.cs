b65b201113b55f05d6106196c799d46fa5393115
sandadev <santhoshkumartalari@outlook.com>
2019-08-26 15:09:31 +0530

    git worked on client

---
+++ Fitnesspro.Service/Controllers/ClientManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Fitnesspro.Infrastructure.Interface.Business;
+using Fitnesspro.Infrastructure.Models;
+
+namespace Fitnesspro.Service.Controllers
+{
+    [RoutePrefix("api/Client")]
+    public class ClientManagerController : ApiController
+    {
+        private IServiceGroupManager _serviceGroupManager;
+    }
+}
---
+++ Fitnesspro/Controllers/ClientController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fitnesspro.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        [Route("ClientList")]
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
